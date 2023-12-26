b6f085a6164ad8bfaac562021fcff49fe1cec6d5
Leonardo-Araujo-Resende <leonardo.a.aguiar@ufv.br>
2023-12-26 14:05:43 -0300

    Excluindo arquivos errados

--- apiIBGE.py
+++
@@ -1,7 +0,0 @@
-import requests
-import pprint
-
-link = "https://servicodados.ibge.gov.br/api/v3/agregados/319/periodos/1996/variaveis/246?localidades=N2[3]"
-
-resposta = requests.get(link).json()
-pprint.pprint(resposta[0]['resultados'][0]['series'])
